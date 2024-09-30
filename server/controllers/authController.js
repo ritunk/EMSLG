a82f7cf848aab8463b52c4504980338b0967b75f
ritik185 <145895219+ritunk@users.noreply.github.com>
2024-09-30 15:50:42 +0530

    auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,7 @@ import bcrypt from "bcrypt";
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User;
+    const user = await User.findOne({ email });
 
     if (!user) {
       res.status(404).json({ success: false, error: "User Not Found" });
@@ -23,6 +23,12 @@ const login = async (req, res) => {
       process.env.JWT_KEY,
       { expiresIn: "10d" }
     );
+
+    res.status(200).json({
+      success: true,
+      token,
+      user: { _id: user._id, name: user.name, role: user.role },
+    });
   } catch (error) {
     console.log(error.message);
   }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.js";
+import connectToDatabase from "./db/db.js";
+
+connectToDatabase();
 
 dotenv.config();
 
